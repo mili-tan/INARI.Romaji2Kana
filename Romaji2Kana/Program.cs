dcead29a9227917798ccab02abb10082fad1f0b8
Milkey Tan <819774989@qq.com>
2018-05-16 10:03:11 +0800

    First

--- Romaji2Kana/Program.cs
+++ Romaji2Kana/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using IniParser.Model;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using IniParser;
+using Microsoft.International.Converters;
 
 namespace Romaji2Kana
 {
     class Program
     {
-        static void Main(string[] args)
+        public static IniData UstData;
+        private static readonly Encoding EncodeJPN = Encoding.GetEncoding("Shift_JIS");
+        private static readonly string UstHeader = "[#VERSION]\r\n" + "UST Version 1.20\r\n";
+
+        static void Main(string[] path)
         {
+            string ustFileStr = File.ReadAllText(string.Join("", path))
+                .Replace(UstHeader, "");
+
+            UstData = new FileIniDataParser().Parser.Parse(ustFileStr);
+
+            UstData.Sections.RemoveSection("#PREV");
+            UstData.Sections.RemoveSection("#NEXT");
+            UstData.Sections.RemoveSection("#SETTING");
+
+            foreach (var itemSection in UstData.Sections)
+            {
+                if (itemSection.Keys["Lyric"] != "R")
+                {
+                    try
+                    {
+                        itemSection.Keys["Lyric"] = KanaConverter.RomajiToHiragana(itemSection.Keys["Lyric"]);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+
+            File.WriteAllText(String.Join("",path), 
+                UstHeader + UstData.ToString().Replace(" = ", "=").Replace("\r\n\r\n", "\r\n"),EncodeJPN);
         }
     }
 }
