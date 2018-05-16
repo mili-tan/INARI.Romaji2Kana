9f473322c62258ba569077aeafa0fe48647d5604
Milkey Tan <819774989@qq.com>
2018-05-16 09:27:04 +0800

    添加项目文件。

---
+++ Romaji2Kana/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Romaji2Kana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
