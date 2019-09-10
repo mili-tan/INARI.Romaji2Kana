b84d083a2d3e3abf7cec27935274a7aed2ed3d47
Milkey Tan <819774989@qq.com>
2019-09-10 23:30:11 +0800

    sth

--- FoxyListener/Program.cs
+++ FoxyListener/Program.cs
@@ -1,18 +1,34 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using IniParser;
+using IniParser.Model;
 using Nancy;
 using Nancy.Hosting.Self;
+using static FoxyListener.Program;
 
 namespace FoxyListener
 {
     static class Program
     {
-        static void Main()
+        public static IniData UstData;
+        public static string UstFileStr;
+        private static readonly Encoding EncodeJPN = Encoding.GetEncoding("Shift_JIS");
+        private static readonly string UstHeader = "[#VERSION]\r\n" + "UST Version 1.20\r\n";
+
+        static void Main(string[] path)
         {
-            Console.WriteLine("Foxy");
-            Console.WriteLine("-------------------");
+            UstFileStr = File.ReadAllText(string.Join("", path), EncodeJPN)
+                .Replace(UstHeader, "");
+            UstData = new FileIniDataParser().Parser.Parse(UstFileStr);
+
+            UstData.Sections.RemoveSection("#PREV");
+            UstData.Sections.RemoveSection("#NEXT");
+            UstData.Sections.RemoveSection("#SETTING");
 
             using (NancyHost host = new NancyHost(new HostConfiguration
-                    { RewriteLocalhost = true, UrlReservations = new UrlReservations { CreateAutomatically = true } },
+                    {RewriteLocalhost = true, UrlReservations = new UrlReservations {CreateAutomatically = true}},
                 new Uri("http://localhost:2020/")))
             {
                 host.Start();
@@ -28,7 +44,23 @@ namespace FoxyListener
     {
         public HomeModule()
         {
-            Get("/", x => "OK");
+            Get("/", x => "Welcome to Foxy");
+            Get("/get", x => UstData.ToString());
+            Get("/get/count", x => UstData.Sections.Count.ToString());
+            Get("/get/full", x => UstFileStr);
+            Get("/get/{section}/{key}", x =>
+            {
+                try
+                {
+                    return new FileIniDataParser().Parser.Parse(UstFileStr)
+                        .Sections["#" + x.section][x.key].ToString();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return e.ToString();
+                }
+            });
         }
     }
 }
