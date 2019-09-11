4825da25510e843e3feff37f56a54eab861d16ea
Milkey Tan <819774989@qq.com>
2019-09-11 19:53:42 +0800

    update set

--- FoxyListener/Program.cs
+++ FoxyListener/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
+using System.Timers;
 using IniParser;
 using IniParser.Model;
 using Nancy;
@@ -14,12 +15,14 @@ namespace FoxyListener
     {
         public static IniData UstData;
         public static string UstFileStr;
-        private static readonly Encoding EncodeJPN = Encoding.GetEncoding("Shift_JIS");
-        private static readonly string UstHeader = "[#VERSION]\r\n" + "UST Version 1.20\r\n";
+        public static string UstFilePath;
+        public static readonly Encoding EncodeJPN = Encoding.GetEncoding("Shift_JIS");
+        public static readonly string UstHeader = "[#VERSION]\r\n" + "UST Version 1.20\r\n";
 
         static void Main(string[] path)
         {
-            UstFileStr = File.ReadAllText(string.Join("", path), EncodeJPN)
+            UstFilePath = string.Join("", path);
+            UstFileStr = File.ReadAllText(UstFilePath, EncodeJPN)
                 .Replace(UstHeader, "");
             UstData = new FileIniDataParser().Parser.Parse(UstFileStr);
 
@@ -37,9 +40,12 @@ namespace FoxyListener
                 Console.WriteLine("Press any [Enter] to close the host.");
                 Console.ReadLine();
             }
+            File.WriteAllText(string.Join("", UstFilePath),
+                UstHeader + UstData.ToString().Replace(" = ", "=").Replace("\r\n\r\n", "\r\n"), EncodeJPN);
         }
     }
 
+    // ReSharper disable once UnusedMember.Global
     public sealed class HomeModule : NancyModule
     {
         public HomeModule()
@@ -48,6 +54,12 @@ namespace FoxyListener
             Get("/get", x => UstData.ToString());
             Get("/get/count", x => UstData.Sections.Count.ToString());
             Get("/get/full", x => UstFileStr);
+            Get("/get/names", x =>
+            {
+                var strs = new List<string>();
+                foreach (var item in UstData.Sections) strs.Add(item.SectionName);
+                return string.Join(Environment.NewLine, strs);
+            });
             Get("/get/{section}/{key}", x =>
             {
                 try
@@ -61,6 +73,99 @@ namespace FoxyListener
                     return e.ToString();
                 }
             });
+            Get("/set/{section}/{key}/{val}", x =>
+            {
+                try
+                {
+                    UstData["#" + x.section][x.key] = x.val.ToString();
+                    return "OK";
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return e.ToString();
+                }
+            });
+            Get("/set/{section}/{key}", x =>
+            {
+                try
+                {
+                    UstData["#" + x.section][x.key] = Request.Query["val"].ToString();
+                    return "OK";
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return e.ToString();
+                }
+            });
+            Get("/set/lyric", x =>
+            {
+                try
+                {
+                    int i = 0;
+                    var lyrics = ((string) Request.Query["val"].ToString()).Split(',');
+                    foreach (var itemSection in UstData.Sections)
+                    {
+                        if (itemSection.Keys["Lyric"] == "R") continue;
+                        itemSection.Keys["Lyric"] = lyrics[i];
+                        i++;
+                    }
+                    return "OK";
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return e.ToString();
+                }
+            });
+            Get("/set/lyric/wq", x =>
+            {
+                try
+                {
+                    int i = 0;
+                    var lyrics = ((string) Request.Query["val"].ToString()).Split(',');
+                    foreach (var itemSection in UstData.Sections)
+                    {
+                        if (itemSection.Keys["Lyric"] == "R") continue;
+                        itemSection.Keys["Lyric"] = lyrics[i];
+                        i++;
+                    }
+
+                    File.WriteAllText(UstFilePath,
+                        UstHeader + UstData.ToString().Replace(" = ", "=").Replace("\r\n\r\n", "\r\n"), EncodeJPN);
+                    Timer t = new Timer(500) {Enabled = true};
+                    t.Elapsed += T_Elapsed;
+                    return "OK";
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return e.ToString();
+                }
+            });
+            Get("/wq", x =>
+            {
+                try
+                {
+                    File.WriteAllText(UstFilePath,
+                        UstHeader + UstData.ToString().Replace(" = ", "=").Replace("\r\n\r\n", "\r\n"), EncodeJPN);
+                    Timer t = new Timer(500) { Enabled = true };
+                    t.Elapsed += T_Elapsed;
+                    return "OK";
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return e.ToString();
+                }
+            });
+        }
+
+        private static void T_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Exit!");
+            Environment.Exit(0);
         }
     }
 }
