d7e56ad97399f8f900cc11db0252b80ec2f50930
Milkey Tan <819774989@qq.com>
2019-09-13 20:06:04 +0800

    JpnImporter

---
+++ JpnImporter/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace JpnImporter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox.Font = new System.Drawing.Font("MS Mincho", 12F);
+            this.richTextBox.Location = new System.Drawing.Point(14, 16);
+            this.richTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(651, 296);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "";
+            // 
+            // button
+            // 
+            this.button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button.Location = new System.Drawing.Point(578, 319);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(87, 30);
+            this.button.TabIndex = 1;
+            this.button.Text = "Import";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonConvert.Location = new System.Drawing.Point(12, 319);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(87, 30);
+            this.buttonConvert.TabIndex = 2;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.ButtonConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 360);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.richTextBox);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "JpnImporter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Button buttonConvert;
+    }
+}
+
---
+++ JpnImporter/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace JpnImporter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            new WebClient().DownloadString(@"http://127.0.0.1:2020/set/lyric/wq?r=1&val=" +
+                                           string.Join(",", richTextBox.SelectedText.Trim().ToCharArray()));
+        }
+
+        private void ButtonConvert_Click(object sender, EventArgs e)
+        {
+            var str = richTextBox.Text;
+            richTextBox.Clear();
+            foreach (var item in str.Split('\n'))
+            {
+                var ifeLang = Activator.CreateInstance(Type.GetTypeFromProgID("MSIME.Japan")) as IFELanguage;
+                int hr = ifeLang.Open();
+                if (hr != 0) throw Marshal.GetExceptionForHR(hr);
+                hr = ifeLang.GetPhonetic(item, 1, -1, out var yomigana);
+                if (hr != 0) throw Marshal.GetExceptionForHR(hr);
+                richTextBox.Text += yomigana + Environment.NewLine;
+            }
+        }
+    }
+
+    [ComImport]
+    [Guid("019F7152-E6DB-11d0-83C3-00C04FDDB82E")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IFELanguage
+    {
+        int Open();
+        int Close();
+        int GetJMorphResult(uint dwRequest, uint dwCMode, int cwchInput, [MarshalAs(UnmanagedType.LPWStr)] string pwchInput, IntPtr pfCInfo, out object ppResult);
+        int GetConversionModeCaps(ref uint pdwCaps);
+        int GetPhonetic([MarshalAs(UnmanagedType.BStr)] string @string, int start, int length, [MarshalAs(UnmanagedType.BStr)] out string result);
+        int GetConversion([MarshalAs(UnmanagedType.BStr)] string @string, int start, int length, [MarshalAs(UnmanagedType.BStr)] out string result);
+    }
+}
